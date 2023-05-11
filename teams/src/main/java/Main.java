58fe4049d5f1efe04b6f0879c866dd428c88f0d9
aaagt <example_e_mail@github_space_man.com>
2023-05-11 21:24:35 +0500

    Add national team algorithm

--- teams/src/main/java/Main.java
+++ teams/src/main/java/Main.java
@@ -1,9 +1,10 @@
-import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static final int TEAM_SIZE = 10;
+
+    public static void main(String[] args) {
         int[][] teams = {
                 {45, 31, 24, 22, 20, 17, 14, 13, 12, 10},
                 {31, 18, 15, 12, 10, 8, 6, 4, 2, 1},
@@ -18,13 +19,34 @@ public class Main {
      * Метод для слияния всех команд в одну национальную
      */
     public static int[] mergeAll(int[][] teams) {
-        // to do
+        int[] mergedTeam = teams[0];
+
+        for (int i = 1; i < teams.length; i++) {
+            mergedTeam = merge(mergedTeam, teams[i]);
+        }
+
+        return mergedTeam;
     }
 
     /**
      * Метод для слияния двух команд в одну
      */
     public static int[] merge(int[] teamA, int[] teamB) {
-        // to do
+        int indexA = 0;
+        int indexB = 0;
+
+        int[] mergedTeam = new int[TEAM_SIZE];
+
+        for (int i = 0; i < TEAM_SIZE; i++) {
+            if (teamA[indexA] > teamB[indexB]) {
+                mergedTeam[i] = teamA[indexA];
+                indexA++;
+            } else {
+                mergedTeam[i] = teamB[indexB];
+                indexB++;
+            }
+        }
+
+        return mergedTeam;
     }
 }
