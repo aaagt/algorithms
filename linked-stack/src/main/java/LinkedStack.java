87ed98f3eead495ce2adb9b7274ba073dd5ad9d7
aaagt <example_e_mail@github_space_man.com>
2023-05-13 19:32:45 +0500

    Add linked stack algorithm

--- linked-stack/src/main/java/LinkedStack.java
+++ linked-stack/src/main/java/LinkedStack.java
@@ -1,36 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinkedStack {
     private Node tail; // ссылка на последний добавленный узел (обёртку)
     private int size; // размер стека, т.е. количество элементов в нём
 
     public void push(int value) {
-        Node node = new Node(value); // создаём новый узел
-        if (tail != null) { // если в стеке уже есть элементы
-            node.setPrev(tail); // связываем новый узел с последним
+        Node node = new Node(value);
+        if (tail != null) {
+            node.setPrev(tail);
         }
-        tail = node; // назначаем новый узел последним узлом
-        size++; // увеличиваем счётчик элементов
+        tail = node;
+        size++;
     }
 
     public int pop() {
-        // ваш код
-        // возьмите value из последнего узла
-        // назначьте предыдущий к последнему узлу последним узлом
+        if (isEmpty()) {
+            throw new RuntimeException("Попытка извлечь значение из пустого стэка");
+        }
+
+        int value = tail.getValue();
+        tail = tail.getPrev();
+        size--;
+
+        return value;
     }
 
     public int getSize() {
-        // ваш код
-        // верните размер стека
+        return size;
     }
 
     public boolean isEmpty() {
-        // ваш код
-        // верните ответ на вопрос, не пустой ли стек
+        return getSize() == 0;
     }
 
     public String toString() {
-        // если есть элементы, пройдитесь по связному списку,
-        // выводя элементы.
-        // вывод должен быть в точности как в комментариях к main
-        // при этом этот метод не должен менять стек!
+        if (isEmpty()) {
+            return "EMPTY";
+        }
+
+        List<String> values = new ArrayList<>(getSize());
+        Node cur = tail;
+        while (cur != null) {
+            values.add(String.valueOf(cur.getValue()));
+            cur = cur.getPrev();
+        }
+
+        return String.join(" -> ", values);
     }
 }
